8d75fe6ba5ab50d99887ff730391b9483dd5f512
franga2000 <miha.frangez@gmail.com>
2014-09-16 22:58:58 +0200

    Version 1.5.1
    Changelog:
     - Fixed password config not being in the right place

--- config.php
+++ config.php
@@ -4,10 +4,9 @@ if (!isset($back)) {
     $back = './';
 }
 
-$password = 'CHANGE-THIS' ; //Not implemented yet
-
 //DO NOT EDIT ANYTHING BELOW THIS LINE!
 $config = array_merge(json_decode(file_get_contents($back . 'servers.json'), true), Array(
+    'password' =>       'CHANGE-THIS',
     'columns' =>        2,  //The number of columns of serverson the main page
     'player_columns'=>  2,  //The number of columns of playerson the server page
     'nojava' =>         false,  //The number of columns of playerson the server page
